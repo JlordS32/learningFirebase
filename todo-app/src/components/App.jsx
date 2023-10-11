caef971049fbeaf71b342eede17cb5e66e478661
JlordS32 <jlordsty128@gmail.com>
2023-10-11 12:02:38 +1100

    new proj

---
+++ todo-app/src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Button } from 'bootstrap';
+
+const App = () => {
+	return (
+		<div className='container'>
+			<Button variant='primary'>Hello</Button>
+         <h1>Hello</h1>
+		</div>
+	);
+};
+
+export default App;
