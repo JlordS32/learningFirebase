a1c3a844ea8bfafbaec30bb602cd710ba82cf383
JlordS32 <jlordsty128@gmail.com>
2023-10-12 15:37:18 +1100

    firebase is interesting

--- first-proj/firstProj/src/components/App.jsx
+++ first-proj/firstProj/src/components/App.jsx
@@ -1,164 +1,79 @@
 import React, { useEffect, useLayoutEffect, useState } from 'react';
-import { app, database } from '../firebaseConfig.js';
-import {
-	collection,
-	addDoc,
-	getDocs,
-	updateDoc,
-	doc,
-   deleteDoc,
-} from 'firebase/firestore';
+import { app, storage } from '../firebaseConfig.js';
+import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
 
 const App = () => {
-	const [input, setInput] = useState({});
-	const [data, setData] = useState([]);
-	const collectionRef = collection(database, 'users');
-
-	const handleChange = (e) => {
-		let newInput = {
-			[e.target.name]: e.target.value,
-		};
-
-		setInput({
-			...input,
-			...newInput,
-		});
-	};
+	const [data, setData] = useState({});
+  
 
 	const handleSubmit = () => {
-		addDoc(collectionRef, {
-			email: input.email,
-			password: input.password,
-		})
-			.then(() => {
-				console.log('Data Added!');
-			})
-			.catch((error) => {
-				console.error(error.message);
-			});
+      const storageRef = ref(storage, `images/${data.name}`);
+		const uploadTask = uploadBytesResumable(storageRef, data);
 
-		getData();
+		uploadTask.on(
+			'state_changed',
+			(snapshot) => {
+				const progress =
+					(snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+
+				console.log('Upload is ', +progress + '% done');
+			},
+			(error) => {
+				console.log(error.message);
+			},
+			() => {
+				getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+					console.log('File is available at ', downloadURL);
+				});
+			}
+		);
 	};
-
-	const updateData = async (id, newValue) => {
-		const docToUpdate = doc(database, 'users', id);
-
-		updateDoc(docToUpdate, {
-			email: newValue.email,
-		})
-      .then(() => {
-         console.log('Data Updated!');
-         getData();
-      })
-      .catch((error) => {
-         console.error(error.message);
-      });
-	};
-	const deleteData = async (id) => {
-		const docToUpdate = doc(database, 'users', id);
-
-		deleteDoc(docToUpdate)
-      .then(() => {
-         console.log('Data Deleted!');
-         getData();
-      })
-      .catch((error) => {
-         console.error(error.message);
-      });
-	};
-
-	const getData = async () => {
-		try {
-			const querySnapshot = await getDocs(collectionRef);
-
-			const fetchedData = querySnapshot.docs.map((doc) => {
-				return {
-					id: doc.id,
-					...doc.data(),
-				};
-			});
-
-			setData(fetchedData);
-		} catch (error) {
-			console.error('Error fetching data: ', error);
-		}
-	};
-
-	useLayoutEffect(() => {
-		getData();
-	}, []);
 
 	return (
-		<div className='app-container'>
-			<div>
-				<input
-					type='email'
-					name='email'
-					placeholder='Email'
-					onChange={handleChange}
-				/>
-				<input
-					type='Password'
-					name='password'
-					placeholder='Password'
-					onChange={handleChange}
-				/>
-				<button onClick={handleSubmit}>Submit</button>
-			</div>
-
+		<div
+			className='app-container'
+			style={{
+				display: 'flex',
+				flexDirection: 'column',
+				position: 'absolute',
+				top: '50%',
+				left: '50%',
+				transform: 'translate(-50%, -50%)',
+			}}
+		>
 			<div
 				style={{
 					display: 'flex',
 					gap: '10px',
-					width: '100%',
-					flexWrap: 'wrap',
 				}}
 			>
-				{data.map((item) => {
-					return (
-						<div
-							className='item'
-							style={{
-								border: '1px solid black',
-								width: '200px',
-								textAlign: 'center',
-							}}
-							key={item.id}
-						>
-							<p>{item.email}</p>
-							<p>{item.password}</p>
-							<input
-								type='email'
-								name='email'
-								placeholder='New Value'
-								onChange={handleChange}
-							/>
-							<div
-								style={{
-									display: 'flex',
-									justifyContent: 'space-around',
-									padding: '10px',
-								}}
-							>
-								<button onClick={() => updateData(item.id, input)}>
-									Update
-								</button>
-								<button
-									onClick={() => deleteData(item.id)}
-									style={{
-										backgroundColor: 'red',
-										color: 'white',
-										border: '0',
-                              borderRadius: '3px'
-									}}
-								>
-									Delete
-								</button>
-							</div>
-						</div>
-					);
-				})}
+				<input
+					type='file'
+					name='password'
+					placeholder='Password'
+					onChange={(event) => setData(event.target.files[0])}
+					style={{
+						padding: '10px 40px',
+						border: '1px black solid',
+					}}
+				/>
 			</div>
+
+			<button
+				onClick={handleSubmit}
+				style={{
+					border: '0',
+					backgroundColor: 'black',
+					width: '50%',
+					color: 'white',
+					padding: '1rem',
+					margin: '2rem 0',
+					borderRadius: '10px',
+					alignSelf: 'center',
+				}}
+			>
+				Submit
+			</button>
 		</div>
 	);
 };
