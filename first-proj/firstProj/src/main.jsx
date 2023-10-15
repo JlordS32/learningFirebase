c200d26fb905049aee6e204edfbd1b480fff64f9
JlordS32 <jlordsty128@gmail.com>
2023-10-15 23:26:17 +1100

    simulating a search query using where and like from firestore

--- first-proj/firstProj/src/main.jsx
+++ first-proj/firstProj/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/FireBaseDatabaseRealtime.jsx';
+import App from './components/FireStoreQueries.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
